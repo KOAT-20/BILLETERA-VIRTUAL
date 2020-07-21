d6ad0e8e2be0528d8c06cef1878c24f72345e8a5
Kevin <keviinnalvarez2015@gmail.com>
2020-07-21 17:24:56 -0400

    cambios y pruebas

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render () {
+    return (
+    <header>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Navbar</a>
+        </div>
+      </nav>
+    </header>
+    );
+  }
 }
-
-export default App;
